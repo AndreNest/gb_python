e8ff1c7891767d50d7033495b84f76d78dfe865b
AndreyNesterov <1@nestandre.ru>
2022-10-03 11:12:20 +0600

    updute 2.0

---
+++ sem_04/func.py
@@ -0,0 +1,32 @@
+def get_int_number(input_string) -> int:
+    '''
+    Ввод натурального числа, через консоль.
+    '''
+    try:
+        num = int(input(input_string))
+        return num
+    except(ValueError):
+        print('Ошибка ввода!')
+        return get_int_number(input_string)
+
+def get_float_number(input_string) -> float:
+    '''
+    Ввод вещественного числа, через консоль.
+    '''
+    try:
+        num = float(input(input_string))
+        return num
+    except(ValueError):
+        print('Ошибка ввода!')
+        return get_float_number(input_string)
+
+def factori(num)->int:
+    '''
+     Факториал числа 
+    '''
+    fact = 1
+    for i in range(2, num + 1):
+        fact *= i
+    return fact
+
+
